2e9046bc895161922575a31a4406a376a1090271
EienYuki <D1044182012@gm.lhu.edu.tw>
2018-08-08 11:37:25 +0800

    修正錯誤

--- tool/box_tool.py
+++ tool/box_tool.py
@@ -18,9 +18,15 @@ def approach(data, offset):
 
 	for index in range(len(data)):
 		tmp = [ int(r) for r in data[index][1:] ]
+		conf_type = len(offset[tmp[-1]])
 
-		for index2 in range(4):
-			data[index][index2+1] = tmp[index2] + offset[tmp[-1]][index2]
+		if conf_type == 4:
+			for index2 in range(4):
+				data[index][index2+1] = tmp[index2] + offset[tmp[-1]][index2]
+		elif conf_type == 2:
+			for index2 in range(2):
+				data[index][index2+1] = tmp[index2] + offset[tmp[-1]][index2]
+				data[index][index2+3] = tmp[index2+2] + offset[tmp[-1]][index2]
 		
 		out.append( ' '.join(str(e) for e in data[index]) )
 
@@ -28,16 +34,19 @@ def approach(data, offset):
 
 if __name__ == '__main__':
 	# 可以把自動產生出來的 *.box 數值進行偏移
-	# 陣列代表第幾頁 陣列中的數值分別是 x y width height
+	# 陣列代表第幾頁
+	# 偏移的設定檔寫法有兩種
+	# [10, 10, 0, 0] 左 上 右 下
+	# [10, 10] 左右 上下
 	offset_conf = [
+		[10, 20],
 		[10, 10, 0, 0],
 		[10, 10, 0, 0],
 		[10, 10, 0, 0],
 		[10, 10, 0, 0],
 		[10, 10, 0, 0],
 		[10, 10, 0, 0],
-		[10, 10, 0, 0],
-		[10, 10, 0, 0]
+		[10, 10]
 	]
 
 	path = "chi_tra.dfkai-sb.exp0.box"
