2c7328e2f2eee2a9d5b8f759e2f8da95bab422d4
amenoimi <bmpss92285@gmail.com>
2018-08-09 15:27:38 +0800

    回復

--- tool/bool_bitmap_tool.py
+++ tool/bool_bitmap_tool.py
@@ -1,3 +1,5 @@
+#-*- coding: utf-8 -*-　
+
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
@@ -8,11 +10,12 @@ ret2,thresh2=cv2.threshold(GrayImage,127,255,cv2.THRESH_BINARY_INV)
 ret3,thresh3=cv2.threshold(GrayImage,127,255,cv2.THRESH_TRUNC)
 ret4,thresh4=cv2.threshold(GrayImage,127,255,cv2.THRESH_TOZERO)
 ret5,thresh5=cv2.threshold(GrayImage,127,255,cv2.THRESH_TOZERO_INV)
-titles = ['Gray Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
-images = [GrayImage, thresh1, thresh2, thresh3, thresh4, thresh5]
-for i in range(6):
+thresh6=cv2.GaussianBlur(img,(5,5),127,255)
+titles = ['Gray Image','BINARY','BINARY_INV','TRUNC','TOZERO','GaussianBlur']
+images = [GrayImage, thresh1, thresh2, thresh3, thresh4,thresh6]
+for i in range(len(images)):
    plt.subplot(2,3,i+1),plt.imshow(images[i],'gray')
    plt.title(titles[i])
    plt.xticks([]),plt.yticks([])
-cv2.imwrite('output.tiff', thresh2)
+#cv2.imwrite('output.tiff', thresh2)
 plt.show()
